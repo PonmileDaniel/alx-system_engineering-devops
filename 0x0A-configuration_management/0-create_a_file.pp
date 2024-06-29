f47d2e75a011c19958b5635872fca05947e079df
PonmileDaniel <pomiledaniel@gmail.com>
2024-06-29 13:17:31 +0100

    COnfig

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+#creating a file in /tmp using project
+
+file {'daniel_ndukwe':
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
