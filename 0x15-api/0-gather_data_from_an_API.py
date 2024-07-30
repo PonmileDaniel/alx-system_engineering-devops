13bedd5dc3a267125d1c2cbfc6aeb83206ed2c66
PonmileDaniel <pomiledaniel@gmail.com>
2024-07-30 22:11:30 +0100

    Learning About APi

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Script that uses JSON placeholder API"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    base_url = "https://jsonplaceholder.typicode.com/"
+    
+    user = '{}users/{}'.format(base_url, sys.argv[1])
+    res = requests.get(user)
+    todo_data = res.json()
+    print("Employee {} is done with tasks".format(todo_data.get('name')), end="")
+    
+    todos = '{}todos?userId={}'.format(base_url, sys.argv[1])
+    res = requests.get(todos)
+    tasks = res.json()
+    last_task = []
+    for task in tasks:
+        if task.get('completed') is True:
+            last_task.append(task)
+
+    print("({}/{}):".format(len(last_task), len(tasks)))
+    for task in last_task:
+        print("\t {}".format(task.get("title")))
