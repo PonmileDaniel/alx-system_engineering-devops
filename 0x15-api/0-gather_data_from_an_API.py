05fb95d4734cac5c0ebd1b3b6f7e791d6059a5a5
PonmileDaniel <pomiledaniel@gmail.com>
2024-07-30 22:56:18 +0100

    Added a / to the url

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,7 +4,7 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    base_url = "https://jsonplaceholder.typicode.com"
+    base_url = "https://jsonplaceholder.typicode.com/"
 
     # Fetch user information
     user = '{}users/{}'.format(base_url, sys.argv[1])
