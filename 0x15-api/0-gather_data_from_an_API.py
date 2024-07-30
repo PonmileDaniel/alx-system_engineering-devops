2e6af35959ae06bf59f0324c7592be0a1068a303
PonmileDaniel <pomiledaniel@gmail.com>
2024-07-30 22:48:33 +0100

    MAking correction

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,25 +1,23 @@
 #!/usr/bin/python3
-""" Script that uses JSON placeholder API"""
+""" Script that uses JSON placeholder API """
 import requests
 import sys
-
 
 if __name__ == "__main__":
     base_url = "https://jsonplaceholder.typicode.com"
 
+    # Fetch user information
     user = '{}users/{}'.format(base_url, sys.argv[1])
     res = requests.get(user)
     data = res.json()
     print("Employee {} is done with tasks".format(data.get('name')), end="")
 
+    # Fetch tasks for the user
     todos = '{}todos?userId={}'.format(base_url, sys.argv[1])
     res = requests.get(todos)
     tasks = res.json()
-    last_task = []
-    for task in tasks:
-        if task.get('completed') is True:
-            last_task.append(task)
+    completed_tasks = [task for task in tasks if task.get('completed')]
 
-    print("({}/{}):".format(len(last_task), len(tasks)))
-    for task in last_task:
+    print(" ({}/{}):".format(len(completed_tasks), len(tasks)))
+    for task in completed_tasks:
         print("\t {}".format(task.get("title")))
