05e0f1acc223be0e62b024f7fed0ce860a74ab12
PonmileDaniel <pomiledaniel@gmail.com>
2024-07-31 08:18:43 +0100

    Dictionary

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Exports to-do list information of all employees to JSON format."""
+import json
+import requests
+
+if __name__ == "__main__":
+    base_url = "https://jsonplaceholder.typicode.com/"
+    users = requests.get(base_url + "users").json()
+
+    all_tasks = {}
+    for user in users:
+        user_id = user.get("id")
+        username = user.get("username")
+        todos = requests.get(base_url + "todos", params={"userId": user_id}).json()
+
+        user_tasks = [{
+            "task": todo.get("title"),
+            "completed": todo.get("completed"),
+            "username": username
+        } for todo in todos]
+
+        all_tasks[user_id] = user_tasks
+
+    with open("todo_all_employees.json", "w") as json_file:
+        json.dump(all_tasks, json_file)
