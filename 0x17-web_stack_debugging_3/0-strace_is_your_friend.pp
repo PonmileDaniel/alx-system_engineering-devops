6c6506bb678e06a289ccaeb9f170190d23933b11
PonmileDaniel <pomiledaniel@gmail.com>
2024-08-17 08:04:25 +0100

    Added provider

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # Fix 500 error when a GET HTTP method is requested
 
 exec{'replace':
- command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
- path    => '/usr/local/bin/:/bin/'
+ provider => shell,
+ command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php'
 }
