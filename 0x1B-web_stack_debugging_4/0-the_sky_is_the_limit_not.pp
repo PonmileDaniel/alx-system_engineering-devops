e2eb73c4d9638f090596b0f01665fdf02606fc62
PonmileDaniel <pomiledaniel@gmail.com>
2024-08-23 10:34:00 +0100

    web stack debugging

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,17 +1,14 @@
 # Increase the amountnn of traffic an Nginx server can handle.
 
 # Increase the ULINIT of the default file
-exec {'fix--for-nginx':
-  # Modify the ULINIT value
-	command => '/bin/sed -i "s/15/4096/" /etc/default/nginx',
-  # Specify the path for the sed command
-	path    => '/usr/local/bin/:/bin/',
+exec {'replace':
+	provider => shell,
+	command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+	before	 => Exec['restart'],
+	
 }
 
-# Restart Nginx
-exec {'nginx-restart':
-  # Restart Nginx services
-	command => '/etc/init.d/nginx restart',
-  # Specify the path for the innit.
-	path    => '/etc/init.d/',
+exec {'restart':
+	provider => shell,
+	command  => 'sudo service nginx restart',
 }
