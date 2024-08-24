5b2447305213853dd5d1537f68692ce5a2f8f5f2
PonmileDaniel <pomiledaniel@gmail.com>
2024-08-24 07:30:53 +0100

    Puppet lint

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,12 +1,11 @@
 # Increase the ULINIT of the default file
 exec {'replace':
-	provider => shell,
-	command  => 'sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
-	before	 => Exec['restart'],
-	
+  provider => shell,
+  command  => 'sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+  before   => Exec['restart'],
 }
 
 exec {'restart':
-	provider => shell,
-	command  => 'sudo service nginx restart',
+  provider => shell,
+  command  => 'sudo service nginx restart',
 }
