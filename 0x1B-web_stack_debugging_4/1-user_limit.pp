e924e50d1f7d305adb69c344a06b0c757c0222e7
PonmileDaniel <pomiledaniel@gmail.com>
2024-08-24 07:37:32 +0100

    OPtional

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,11 @@
+# FIx problem of high amount file
+exec {'replace-1':
+  provider => shell,
+  command  => 'sudo sed -i "s/nofile 5/nofile 50000/" /etc/security/limits.conf',
+  before   => Exec['repplace-2'],
+}
+
+exec {'replace-2':
+  provider => shell,
+  command  => 'sudo sed -i "s/nofile 4/nofile 40000/" /etc/security/limits.conf',
+}
