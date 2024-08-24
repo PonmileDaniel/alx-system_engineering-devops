56ecd6305286996c79e699a0754ce56ae7c5d678
PonmileDaniel <pomiledaniel@gmail.com>
2024-08-24 07:39:16 +0100

    OPtional

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -2,7 +2,7 @@
 exec {'replace-1':
   provider => shell,
   command  => 'sudo sed -i "s/nofile 5/nofile 50000/" /etc/security/limits.conf',
-  before   => Exec['repplace-2'],
+  before   => Exec['replace-2'],
 }
 
 exec {'replace-2':
